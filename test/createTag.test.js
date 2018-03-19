1eab5d0ae9998289b2d4918825d7d2e37d91faaf
Cristian Vargas <cristianv975@gmail.com>
2018-03-19 12:36:32 -0500

    test: create tag

--- test/createTag.test.js
+++ test/createTag.test.js
@@ -2,10 +2,20 @@
 
 const createTag = require('../src/createTag')
 
-test('create dom element', () => {
+test('create <a>Hola mundo</a>', () => {
   const link = createTag('a', 'Hola mundo')
 
   expect(link.tagName).toEqual('A')
   expect(link.innerHTML).toEqual('Hola mundo')
   expect(link.childElementCount).toBe(0)
+})
+
+test('create <li><a>Hola Mundo</a></li>', () => {
+  const link = createTag('a', 'Hola Mundo')
+  const li = createTag('li', link)
+
+  expect(li.tagName).toEqual('LI')
+  expect(li.childElementCount).toBe(1)
+  expect(li.firstElementChild.tagName).toEqual('A')
+  expect(li.firstElementChild.innerHTML).toEqual('Hola Mundo')
 })
